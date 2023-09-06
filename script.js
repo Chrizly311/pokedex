f05a9a3cf837dd9a95a3765d12ae7f349968587f
Chrizly311 <117831221+Chrizly311@users.noreply.github.com>
2023-09-06 20:34:34 +0200

    first part for about-section

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ async function loadPokemon() {
     console.log('Loaded Pokemon', currentPokemon);
 
     renderPokemonInfo();
+    sectionAbout();
 }
 
 
@@ -16,4 +17,21 @@ function renderPokemonInfo() {
     document.getElementById('pokeNumber').innerHTML = `#00${currentPokemon['id']}`;
     document.getElementById('pokeType').innerHTML = currentPokemon['types']['0']['type']['name'];
     document.getElementById('pokemon-img').src = `${currentPokemon['sprites']['other']['official-artwork']['front_default']}`;
+}
+
+function sectionAbout() {
+    document.getElementById('section-about').innerHTML = /*html*/`
+    <div class="species flex-container">
+        <div>Species:</div><div>${currentPokemon['types']['0']['type']['name']}</div>
+    </div>
+    <div class="height flex-container">
+        <div>Height:</div><div>${currentPokemon['height']}"</div>
+    </div>
+    <div class="weight flex-container">
+        <div>Height:</div><div>${currentPokemon['weight']}lbs</div>
+    </div>
+    <div class="abilities flex-container">
+        <div>Abilities:</div><div>${currentPokemon['abilities']['0']['ability']['name']}</div>
+    </div>
+    `;
 }
