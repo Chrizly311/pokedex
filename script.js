b773a316f802d1e9ad9b250127c332f8f1371fb9
Chrizly311 <117831221+Chrizly311@users.noreply.github.com>
2023-09-06 20:45:52 +0200

    enhanced about-section

--- script.js
+++ script.js
@@ -22,16 +22,16 @@ function renderPokemonInfo() {
 function sectionAbout() {
     document.getElementById('section-about').innerHTML = /*html*/`
     <div class="species flex-container">
-        <div>Species:</div><div>${currentPokemon['types']['0']['type']['name']}</div>
+        <div>Species</div><div>${currentPokemon['types']['0']['type']['name']}</div>
     </div>
     <div class="height flex-container">
-        <div>Height:</div><div>${currentPokemon['height']}"</div>
+        <div>Height</div><div>${currentPokemon['height']}"</div>
     </div>
     <div class="weight flex-container">
-        <div>Height:</div><div>${currentPokemon['weight']}lbs</div>
+        <div>Height</div><div>${currentPokemon['weight']}lbs</div>
     </div>
     <div class="abilities flex-container">
-        <div>Abilities:</div><div>${currentPokemon['abilities']['0']['ability']['name']}</div>
+        <div>Abilities</div><div>${currentPokemon['abilities']['0']['ability']['name']}</div>
     </div>
     `;
 }
